c086d034ee4bc78d100551ba447eb60f6746a0b5
Thomas P <tpreston013@gmail.com>
2022-01-24 19:21:34 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# readme_generator
+
+## ReadMe Generator
+
+#Description
+An application to create a ReadMe.md file. 
+
+##Installation
+instructions
+
+##Usage
+details
+
+#Contribute
+asdfjkl;
+
+##Testing
+instructions
---
+++ index.js
@@ -0,0 +1,80 @@
+// Packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+// Array of questions for user input
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'projectName',
+            message: 'What is the title of your project?',
+        },
+        {
+            type: 'input',
+            name: 'projectDescription',
+            message: 'Please provide a description of the project.',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Please provide instructions for installation.',
+        },
+        {
+            type: 'input',
+            name: 'usageInfo',
+            message: 'Please provide details about the project usage.',
+        },
+        {
+            type: 'input',
+            name: 'contributionGuidelines',
+            message: 'What are the contribution guidelines?',
+        },
+        {
+            type: 'input',
+            name: 'testInstructions',
+            message: 'Please provide test instructions.',
+        },
+    
+    ]);
+};
+
+
+const generateReadme = ({
+    projectName,
+    projectDescription,
+    installation,
+    usageInfo,
+    contributionGuidelines,
+    testInstructions
+}) => `
+## ${projectName}
+
+#Description
+${projectDescription}
+
+##Installation
+${installation}
+
+##Usage
+${usageInfo}
+
+#Contribute
+${contributionGuidelines}
+
+##Testing
+${testInstructions}
+`
+
+// Function to initialize app
+
+const init = () => {
+    questions()
+    .then((userInput)=> fs.writeFileSync(`Readme.md`, generateReadme(userInput)))
+    .catch((err) => {
+        throw err
+    })
+};
+
+// Function call to initialize app
+init();
